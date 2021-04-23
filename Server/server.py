75b12011008d49c88c4f535dbb4e1fb1bb1be41d
Vitali Dambajev <vitali.dambajev@insly.com>
2021-04-23 17:06:56 +0300

    Added file with homeassignment

---
+++ Server/server.py
@@ -0,0 +1,27 @@
+import socket
+import random
+
+FORMAT = "UTF-8"
+LOCALHOST = "127.0.0.1"
+
+
+def random_port():
+    return random.randint(20000, 30000)
+
+
+my_socket = socket.socket()
+
+addr_and_port = (LOCALHOST, random_port())
+my_socket.bind(addr_and_port)
+print(f"Started socket on {addr_and_port}")
+
+my_socket.listen(10)
+
+conn, addr = my_socket.accept()
+print("Got connection", conn, addr)
+data = conn.recv(1024)
+print("Got data:\n ", data.decode("utf-8"))
+
+conn.send(f"HTTP/1.1 200 OK\n Content-Lenght: 100\n Content-Type: text/html\n\n {data.decode(FORMAT)}".encode(FORMAT))
+
+my_socket.close()
